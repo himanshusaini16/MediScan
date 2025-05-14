70535c7311b0f226bbaf5d09ce1258716127350d
Himanshu Saini <himanshusaini025@gmail.com>
2025-05-15 01:25:28 +0530

    fixed

--- Admin/src/pages/Doctor/DoctorChat.jsx
+++ Admin/src/pages/Doctor/DoctorChat.jsx
@@ -254,7 +254,8 @@ const DoctorChatPage = () => {
   <div className="hidden sm:block border-r overflow-y-auto pr-2 w-full sm:w-1/4">
     <h2 className="text-xl font-bold mb-4 pl-2">Patients</h2>
     {uniqueuser.slice().reverse().map((item, index) => {
-      const roomToken = createRoomToken(profileData._id,item.userId);
+      const roomToken = createRoomToken(item.userId,profileData._id);
+
       console.log("room token",roomToken)
       console.log("itemid",item.userId)
       console.log("ProfileDataId",profileData._id)
