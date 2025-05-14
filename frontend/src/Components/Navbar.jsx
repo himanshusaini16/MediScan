d3e158bd30f5b1a445244eaa4bd5350e4956f7db
Himanshu Saini <himanshusaini025@gmail.com>
2025-05-15 04:06:04 +0530

    navbar

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -90,7 +90,7 @@ const Navbar = () => {
         {/* Mobile Menu */}
         <div className={`${showMenu ? 'fixed w-full' : 'h-0 w-0'} md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all`}>
           <div className="flex items-center justify-between px-5 py-6">
-            <img className="w-36" src={assets.logo} alt="Logo" />
+            <img className="w-30" src='/logo.png' alt="Logo" />
             <img className="w-7" onClick={() => setShowMenu(false)} src={assets.cross_icon} alt="Close" />
           </div>
           <ul className="flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium">
