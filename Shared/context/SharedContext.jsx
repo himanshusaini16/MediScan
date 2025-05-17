d3635c6ff6c4a266bf8cb7984d9846c396f99568
Himanshu Saini <himanshusaini025@gmail.com>
2025-05-17 05:48:05 +0530

    Shared Context

---
+++ Shared/context/SharedContext.jsx
@@ -0,0 +1,35 @@
+import { io } from "socket.io-client";
+
+
+export const SharedContext = createContext();
+
+
+const SharedContextProvider = (props) => {
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+console.log("backend url from shared context",backendUrl)
+const [socket, setSocket] = useState(null);
+
+useEffect(() => {
+    const newSocket = io(backendUrl, {
+      withCredentials: true,
+      transports: ["websocket"],
+    });
+    setSocket(newSocket);
+
+    return () => newSocket.close();
+  }, [backendUrl]);
+
+
+  const value = {
+    socket
+  }
+
+   return (
+      <SharedContextProvider.Provider value={value}>{props.children}</SharedContextProvider.Provider>
+    );
+
+}
+
+
+export default SharedContextProvider;
