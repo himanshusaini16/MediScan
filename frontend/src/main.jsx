1bb39a84cb08fde3c55b0f59aa55f176d58f1a59
Himanshu Saini <himanshusaini025@gmail.com>
2025-05-17 05:49:44 +0530

    frontend'

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,14 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import AppContextProvider from './context/AppContext.jsx'
+import SharedContextProvider from '../../Shared/context/SharedContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
+  <SharedContextProvider>
   <AppContextProvider>
     <App />
   </AppContextProvider>
+  </SharedContextProvider>
   </BrowserRouter>,
 )
